b2a639c5cc0e307ce309f0f4c79297c1cacaa7cc
kv-2 <str_token@mail.ru>
2019-07-16 00:30:58 +0300

    ng g component pomodoro-timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppComponent } from "./app.component";
+import { PomodoroTimerComponent } from './pomodoro-timer/pomodoro-timer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PomodoroTimerComponent],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
