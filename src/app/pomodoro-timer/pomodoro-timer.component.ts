f7f28d82f3ce0e08d521f2af6329f467c3ba7156
kv-2 <str_token@mail.ru>
2019-07-16 01:41:53 +0300

    update controller and view component

--- src/app/pomodoro-timer/pomodoro-timer.component.ts
+++ src/app/pomodoro-timer/pomodoro-timer.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-pomodoro-timer',
-  templateUrl: './pomodoro-timer.component.html',
-  styleUrls: ['./pomodoro-timer.component.css']
+  selector: "pomodoro-timer",
+  templateUrl: "./pomodoro-timer.component.html",
+  styleUrls: ["./pomodoro-timer.component.css"]
 })
-export class PomodoroTimerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class PomodoroTimerComponent {
+  minutes: number;
+  seconds: number;
+  constructor() {
+    this.minutes = 24;
+    this.seconds = 59;
   }
-
 }
