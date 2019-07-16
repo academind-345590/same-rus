17a63c5163900102c30518ad6db6bd1da69c6a67
kv-2 <str_token@mail.ru>
2019-07-16 16:40:37 +0300

    add button start-pause

--- src/app/pomodoro-timer/pomodoro-timer.component.ts
+++ src/app/pomodoro-timer/pomodoro-timer.component.ts
@@ -8,24 +8,38 @@ import { Component } from "@angular/core";
 export class PomodoroTimerComponent {
   minutes: number;
   seconds: number;
+  isPaused: boolean;
+  buttonLabel: string;
+  
   constructor() {
-    this.minutes = 24;
-    this.seconds = 59;
+    this.resetPomodoro();
     // вызов обратного отсчёта
     setInterval(()=> this.tick(), 1000);
 
   }
   resetPomodoro(): void {
-    
+    this.minutes = 24;
+    this.seconds = 59;
+	this.buttonLabel = 'Start';
+	this.togglePause();
   }
   // обратный отсчёт
   private tick (): void {
-    if (--this.seconds < 1) {
-      this.seconds = 59;
-      if (--this.minutes) {
-        this.minutes = 24;
-        this.seconds = 59;
-      }
-    }
+	if (!this.isPaused){
+		if (--this.seconds < 0) {
+		  this.seconds = 59;
+		  if (--this.minutes < 0) {
+			this.resetPomodoro();
+		  }
+		}
+	}
+  }
+  //изиеняет значение пременной id{\Paused
+  togglePause(): void {
+	  this.isPaused = !this.isPaused;
+	  // если начат обратный отчт
+	  if (this.minutes < 24 || this.seconds < 59 ) {
+		  this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+	  }
   }
 }
