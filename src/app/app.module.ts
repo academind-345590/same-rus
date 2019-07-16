281cef3da06c7bc220f13f462a81f9c748515ce0
kv-2 <str_token@mail.ru>
2019-07-16 19:57:21 +0300

    update view with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from "@angular/core";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppComponent } from "./app.component";
 import { PomodoroTimerComponent } from './pomodoro-timer/pomodoro-timer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, PomodoroTimerComponent],
+  declarations: [
+      AppComponent,
+      PomodoroTimerComponent,
+      NavbarComponent,
+  ],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
